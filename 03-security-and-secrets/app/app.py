a1994bbde653ec66b89c5ec0adcf082e4e6f057f
Akash_Deshmukh <akash13.xworkz@gmail.com>
2026-07-16 23:27:22 +0530

    Add secure pipeline

--- 03-security-and-secrets/app/app.py
+++ 03-security-and-secrets/app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"API_KEY: {os.getenv('API_KEY')}"
+    return f"API KEY: {os.getenv('API_KEY')}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
