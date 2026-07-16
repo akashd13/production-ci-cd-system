c5f1b172251b42ab6c55e80796c12ba2580aa8cd
Akash_Deshmukh <akash13.xworkz@gmail.com>
2026-07-16 23:09:32 +0530

    Updated the app code

--- 02-ci-build-and-test/app/app.py
+++ 02-ci-build-and-test/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CICD Prod Runningg"
+    return "CICD Prod Running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
