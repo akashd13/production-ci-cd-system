5eb9e668d31590ffc93c2b8d908e7f952aa50e22
Akash_Deshmukh <akash13.xworkz@gmail.com>
2026-07-16 23:18:49 +0530

    Add secure pipeline

--- 03-security-and-secrets/app/app.py
+++ 03-security-and-secrets/app/app.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Application is running with Secrets Management"
+    return f"API KEY: {os.getenv('API_KEY')}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
